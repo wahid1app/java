d112a641d6fc9cc20bca75ed344accd0c6652203
wahid1app <114626336+wahid1app@users.noreply.github.com>
2022-09-29 20:13:14 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-alert("ahmaaad");
+console.log('outside');
+
+document.addEventListener(
+  'load',
+  (() => {console.log('inside iif  load addEvenetListtern');})())
+  
+  document.addEventListener(
+  'DOMContentLoaded',
+  (() => {console.log('inside iif DOMContentLoaded addEvenetListtern');})())
+  
+  document.addEventListener(
+  'load',
+  () => {console.log('inside load addEvenetListtern');})
+  
+  document.addEventListener(
+  'DOMContentLoaded',
+  () => {console.log('inside DOMContentLoaded addEvenetListtern');})
